7954650bc49777a797d1d6afafe932f42a2eb116
Stephen M. Walker II <swalker@onehub.com>
2008-08-14 15:21:51 -0700

    + link_to instead of link + alternate content tag method

--- obfuscated_email_helper.rb
+++ obfuscated_email_helper.rb
@@ -20,14 +20,12 @@ module ObfuscatedEmailHelper
     obfuscated_link(title || email, "mailto:#{email}", options)
   end
   alias obfuscated_mail_to obfuscated_mail
-  
+
   def obfuscated_link(title, href = "", options = {})
     chars, random = [], rand(255)
-    plain = link(title, href, options)
+    plain = link_to(title, href, options)
     plain.each_byte { |c| chars << "#{c + random}-#{random}" }
-    tag(:script, :type => "text/javascript") do
-      "document.write(String.fromCharCode(#{chars.join(',')}));"
-    end
+    content_tag(:script, "document.write(String.fromCharCode(#{chars.join(',')}));", :type => "text/javascript")
   end
   alias obfuscated_link_to obfuscated_link
 end
